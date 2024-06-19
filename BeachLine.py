ab18bd318fc01bd7ad4e93a25decf572b6dca770
john <johnsnadden1@gmail.com>
2024-06-18 19:09:10 -0500

    FortuneTree insertion and deletion algorithms seem to be working correctly. Also added graphviz visualisation, like in the AVLTree class

--- BeachLine.py
+++ BeachLine.py
@@ -8,333 +8,376 @@ DEFAULT = object()
 
 # encodes, and provides an interface for, parabolics arcs with fixed focus, and a variable (horizontal) directrix
 class Arc:
-    def __init__(self, focus):
+	def __init__(self, focus):
 
-        # geometric data
-        self.focus = focus
-        self.event = None
-        self.left_halfedge = None
-        self.right_halfedge = None
+		# geometric data
+		self.focus = focus
+		self.event = None
+		self.left_halfedge = None
+		self.right_halfedge = None
 
-        # data for tree structure
-        self.parent = None
-        self.left = None
-        self.right = None
-        self.previous = None
-        self.next = None
-        self.height = 0
+		# data for tree structure
+		self.parent = None
+		self.left = None
+		self.right = None
+		self.previous = None
+		self.next = None
+		self.height = 0
 
-    # produce a list of the coefficients for the associated quadratic function, given the current directrix y-value
-    def Coefficents(self, directrix):
-        a = 1 / (2 * (self.focus[1] - directrix))
-        b = -2 * a * self.focus[0]
-        c = a * (self.focus[0]**2 + self.focus[1]**2 - directrix**2)
-        return [a,b,c]
+	# produce a list of the coefficients for the associated quadratic function, given the current directrix y-value
+	def Coefficents(self, directrix):
+		a = 1 / (2 * (self.focus[1] - directrix))
+		b = -2 * a * self.focus[0]
+		c = a * (self.focus[0]**2 + self.focus[1]**2 - directrix**2)
+		return [a,b,c]
 
-    # evaluate value of the quadratic at x, given the current directrix y-value
-    def Evaluate(self, x, directrix):
-        return ((x - self.focus[0])**2 / (2 * (self.focus[1] - directrix))) + ((self.focus[1] + directrix) / 2)
+	# evaluate value of the quadratic at x, given the current directrix y-value
+	def Evaluate(self, x, directrix):
+		return ((x - self.focus[0])**2 / (2 * (self.focus[1] - directrix))) + ((self.focus[1] + directrix) / 2)
 
-    # returns a list of all (real, internal) intersection points (in list form [x,y]) between two parabolic arcs
-    def Intersections(self, other, directrix):
-        # get the coefficients and form a difference of quadratic functions
-        A = self.coefficents(directrix)[0] - other.coefficents(directrix)[0]
-        B = self.coefficents(directrix)[1] - other.coefficents(directrix)[1]
-        C = self.coefficents(directrix)[2] - other.coefficents(directrix)[2]
-        discriminant = B**2 - 4*A*C
-        out = []
-        
-        if A == 0:
-            if B==0:
-                if C==0:
-                    print("Infinitely many intersections: parabolae overlap on an interval.", file=sys.stderr)
-                    return None
+	# returns a list of all (real, internal) intersection points (in list form [x,y]) between two parabolic arcs
+	def Intersections(self, other, directrix):
+		# get the coefficients and form a difference of quadratic functions
+		A = self.coefficents(directrix)[0] - other.coefficents(directrix)[0]
+		B = self.coefficents(directrix)[1] - other.coefficents(directrix)[1]
+		C = self.coefficents(directrix)[2] - other.coefficents(directrix)[2]
+		discriminant = B**2 - 4*A*C
+		out = []
 
-            else: # the parabolae are horizontal translations of one another, and have a single intersection
-                out.append(-C/B)
+		if A == 0:
+			if B==0:
+				if C==0:
+					print("Infinitely many intersections: parabolae overlap on an interval.", file=sys.stderr)
+					return None
 
-        else:
-            if discriminant == 0: # double root
-                intersection_x = -B / (2 * A)
-                out.append(intersection_x)
-            
-            elif discriminant > 0: # distinct real roots
-                intersection_x1 = (-B + math.sqrt(discriminant))/(2*A)
-                intersection_x2 = (-B - math.sqrt(discriminant))/(2*A)
-                out.append(intersection_x1)
-                out.append(intersection_x2)
-        
-        return out
+			else: # the parabolae are horizontal translations of one another, and have a single intersection
+				out.append(-C/B)
+
+		else:
+			if discriminant == 0: # double root
+				intersection_x = -B / (2 * A)
+				out.append(intersection_x)
+
+			elif discriminant > 0: # distinct real roots
+				intersection_x1 = (-B + math.sqrt(discriminant))/(2*A)
+				intersection_x2 = (-B - math.sqrt(discriminant))/(2*A)
+				out.append(intersection_x1)
+				out.append(intersection_x2)
+
+		return out
 
 
 
 class FortuneTree: # basically a combination of an AVL tree and a doubly-linked list, with parabolic arcs at each node
-    def __init__(self, arc):
+	def __init__(self, arc):
 
-        self.root = arc      
+		self.root = arc
 
-    def GetHeight(self, arc):
+	def GetHeight(self, arc):
 
-        if arc == None:
-            return -1
-        
-        return arc.height
+		if arc == None:
+			return -1
 
-    def GetBalance(self, arc):
+		return arc.height
 
-        return self.GetHeight(arc.left) - self.GetHeight(arc.right)
+	def GetBalance(self, arc):
 
-    def RecalculateHeight(self, arc):
+		return self.GetHeight(arc.left) - self.GetHeight(arc.right)
 
-        if arc == None:
-            return -1
-        
-        return 1 + max(self.GetHeight(arc.left), self.GetHeight(arc.right))
+	def RecalculateHeight(self, arc, recurse=False):
+		if arc == None:
+			return -1
 
-    def RotateRight(self, arc):
+		if recurse:
+			return 1 + max(self.RecalculateHeight(arc.left, True), self.RecalculateHeight(arc.right, True))
 
-        x = arc.left
-        y = x.right
-        
-        # do the rotation
-        x.right = arc
-        x.parent = arc.parent
-        arc.parent = x
-        arc.left = y
-        if y != None:
-            y.parent = arc
-        
-        # recalculate heights
-        self.RecalculateHeight(arc)
-        self.RecalculateHeight(x)
+		else:
+			return 1 + max(self.GetHeight(arc.left), self.GetHeight(arc.right))
 
-        return x
-    
-    def RotateLeft(self, arc):
+	def RotateRight(self, arc):
 
-        x = arc.right
-        y = x.left
-        
-        # do the rotation
-        x.left = arc
-        x.parent = arc.parent
-        arc.parent = x
-        arc.right = y
-        if y != None:
-            y.parent = arc
-        
-        # recalculate heights
-        self.RecalculateHeight(arc)
-        self.RecalculateHeight(x)
+		x = arc.left
+		y = x.right
 
-        return x
+		# do the rotation
+		x.right = arc
+		x.parent = arc.parent
+		arc.parent = x
+		arc.left = y
+		if y != None:
+			y.parent = arc
 
-    def Rebalance(self, arc, balance):
+		# recalculate heights
+		arc.height = self.RecalculateHeight(arc)
+		x.height = self.RecalculateHeight(x)
 
-        if balance > 1:
-            # left-heavy
-            x = arc.left
-            
-            if self.GetBalance(x) < 0:
-                # do left-right rotation
-                arc.left = self.RotateLeft(x)
-                arc = self.RotateRight(arc)
-            else:
-                # just do right rotation
-                arc = self.RotateRight(arc)
-        
-        if balance < -1:
-            # right-heavy
-            x = arc.right
-            
-            if self.GetBalance(x) > 0:
-                # do right-left rotation
-                arc.right = self.RotateRight(x)
-                arc = self.RotateLeft(arc)
-            else:
-                # do left rotation
-                arc = self.RotateLeft(arc)
+		return x
 
-        return self.GetBalance(arc)
+	def RotateLeft(self, arc):
 
-    def FixSubtree(self, arc = DEFAULT):
+		x = arc.right
+		y = x.left
 
-        if arc == DEFAULT:
-            arc = self.root
+		# do the rotation
+		x.left = arc
+		x.parent = arc.parent
+		arc.parent = x
+		arc.right = y
+		if y != None:
+			y.parent = arc
 
-        if arc == None:
-            return
-        
-        self.FixSubtree(arc.left)
-        self.FixSubtree(arc.right)
-        
-        balance = self.GetBalance(arc)
+		# recalculate heights
+		arc.height = self.RecalculateHeight(arc)
+		x.height = self.RecalculateHeight(x)
 
-        while abs(balance) > 1:
-            balance = self.Rebalance(arc, balance)
-        
-        return
+		return x
 
-    def Min(self, subtree):
+	def Rebalance(self, arc, balance):
 
-        arc = subtree
+		root = (arc == self.root)
 
-        while arc.left != None:
-            arc = arc.left
-        
-        return arc
+		if balance > 1:
+			# left-heavy
+			x = arc.left
 
-    def Max(self, subtree):
-        
-        arc = subtree
+			if self.GetBalance(x) < 0:
+				# do left-right rotation
+				arc.left = self.RotateLeft(x)
+				arc = self.RotateRight(arc)
+			else:
+				# just do right rotation
+				arc = self.RotateRight(arc)
 
-        while arc.right != None:
-            arc = arc.right
-        
-        return arc
+		if balance < -1:
+			# right-heavy
+			x = arc.right
 
-    def Successor(self, arc):
+			if self.GetBalance(x) > 0:
+				# do right-left rotation
+				arc.right = self.RotateRight(x)
+				arc = self.RotateLeft(arc)
+			else:
+				# do left rotation
+				arc = self.RotateLeft(arc)
 
-        if arc.right != None:
-            return self.Min(arc.right)
-                
-        successor = arc
+		if root:
+			self.root = arc
 
-        while successor.parent != None:
-            if successor == successor.parent.left:
-                return successor.parent
-            successor = successor.parent
-        
-        return None
-    
-    def Predecessor(self, arc):
+		return arc
 
-        if arc.left != None:
-            return self.Max(arc.left)
-                
-        predecessor = arc
+	def FixTree(self, arc = DEFAULT):
 
-        while predecessor.parent != None:
-            if predecessor == predecessor.parent.right:
-                return predecessor.parent
-            predecessor = predecessor.parent
-        
-        return None
+		if arc == DEFAULT:
+			arc = self.root
 
-    def InsertBefore(self, arc, newarc):
+		if arc == None:
+			return
 
-        if arc.left == None:
-            arc.left = newarc
-            newarc.parent = arc
-        
-        else:
-            pred = self.Predecessor(arc)
-            pred.right = newarc
-            newarc.parent = pred
-        
-        newarc.previous = arc.previous
-        newarc.next = arc
-        if newarc.previous != None:
-            newarc.previous.next = newarc
-        arc.previous = newarc
-        
-        self.FixSubtree()
-        return
-    
-    def InsertAfter(self, arc, newarc):
+		arc.left = self.FixTree(arc.left)
+		arc.right = self.FixTree(arc.right)
 
-        if arc.right == None:
-            arc.right = newarc
-            newarc.parent = arc
-        
-        else:
-            succ = self.Successor(arc)
-            succ.left = newarc
-            newarc.parent = succ
-        
-        newarc.next = arc.next
-        newarc.previous = arc
-        if newarc.next != None:
-            newarc.next.previous = newarc
-        arc.next = newarc
-        
-        self.FixSubtree()
-        return
-    
-    def Replace(self, arc, replacement):
+		balance = self.GetBalance(arc)
 
-        if arc.parent == None:
-            self.root = replacement
-            replacement.parent = None
-        elif arc == arc.parent.left:
-            arc.parent.left = replacement
-            replacement.parent = arc.parent
-        else:
-            arc.parent.right = replacement
-            replacement.parent = arc.parent
+		while abs(balance) > 1:
+			arc = self.Rebalance(arc, balance)
+			balance = self.GetBalance(arc)
+		
+		arc.height = self.RecalculateHeight(arc)
 
-        return
+		return arc
 
-    def Delete(self, arc):
+	def Min(self, subtree=DEFAULT):
 
-        if arc.left == None:
-            replacement = arc.right
-            self.Replace(arc, arc.right)
-        
-        elif arc.right == None:
-            replacement = arc.left
-            self.Replace(arc, arc.left)
-        
-        else: # node has two children, crown its successor!
-            replacement = arc.next
-            
-            if replacement.parent != arc:
-                self.Replace(replacement, replacement.right)
-                replacement.right = arc.right
-                arc.right.parent = replacement
-            
-            self.Replace(arc, replacement)
-            replacement.left = arc.left
-            arc.left.parent = replacement
+		if subtree == DEFAULT:
+			subtree = self.root
 
-        if arc.previous != None:
-            arc.previous.next = arc.next
-        if arc.next != None:
-            arc.next.previous = arc.previous
+		arc = subtree
 
-        arc = None
-        self.FixSubtree()
+		while arc.left != None:
+			arc = arc.left
 
-        return replacement
+		return arc
 
-    # generates a pdf file containing a plot of the tree, via the graphviz package
-    def PlotTree(self):
-        current_time = time.time()
-        # TODO: fix this
-        # nodes = self.list_tree()
-        # dot = graphviz.Digraph()
+	def Max(self, subtree=DEFAULT):
 
-        # for node in nodes:
-        #     dot.node(f"{node.value}")
+		if subtree == DEFAULT:
+			subtree = self.root
 
-        #     if node.left_child != None:
-        #         dot.edge(f"{node.value}",f"{node.left_child.value}")
+		arc = subtree
 
-        #     if node.right_child != None:
-        #         dot.edge(f"{node.value}",f"{node.right_child.value}")
-        
-        # file_path = f"graphviz_outputs/avl_tree_{current_time}"
-        # dot.render(file_path, cleanup=True)
+		while arc.right != None:
+			arc = arc.right
+
+		return arc
+
+	def Successor(self, arc):
+
+		if arc.right != None:
+			return self.Min(arc.right)
+
+		successor = arc
+
+		while successor.parent != None:
+			if successor == successor.parent.left:
+				return successor.parent
+			successor = successor.parent
+
+		return None
+
+	def Predecessor(self, arc):
+
+		if arc.left != None:
+			return self.Max(arc.left)
+
+		predecessor = arc
+
+		while predecessor.parent != None:
+			if predecessor == predecessor.parent.right:
+				return predecessor.parent
+			predecessor = predecessor.parent
+
+		return None
+
+	def InsertBefore(self, arc, newarc):
+
+		if arc.left == None:
+			arc.left = newarc
+			newarc.parent = arc
+
+		else:
+			pred = self.Predecessor(arc)
+			pred.right = newarc
+			newarc.parent = pred
+
+		self.root = self.FixTree()
+
+		newarc.previous = arc.previous
+		newarc.next = arc
+
+		if newarc.previous != None:
+			newarc.previous.next = newarc
+
+		arc.previous = newarc
+
+		return
+
+	def InsertAfter(self, arc, newarc):
+
+		if arc.right == None:
+			arc.right = newarc
+			newarc.parent = arc
+
+		else:
+			succ = self.Successor(arc)
+			succ.left = newarc
+			newarc.parent = succ
+
+		self.root = self.FixTree()
+
+		newarc.next = arc.next
+		newarc.previous = arc
+
+		if newarc.next != None:
+			newarc.next.previous = newarc
+
+		arc.next = newarc
+
+		return
+
+	def Replace(self, arc, replacement):
+
+		if arc.parent == None:
+			self.root = replacement	
+		elif arc == arc.parent.left:
+			arc.parent.left = replacement	
+		else:
+			arc.parent.right = replacement
+		
+		if replacement != None:
+			replacement.parent = arc.parent
+
+		return
+
+	def Delete(self, arc):
+
+		if arc.left == None:
+			replacement = arc.right
+			self.Replace(arc, arc.right)
+
+		elif arc.right == None:
+			replacement = arc.left
+			self.Replace(arc, arc.left)
+
+		else: # node has two children, crown its successor!
+			replacement = arc.next
+
+			if replacement.parent != arc:
+				self.Replace(replacement, replacement.right)
+				replacement.right = arc.right
+				arc.right.parent = replacement
+
+			self.Replace(arc, replacement)
+			replacement.left = arc.left
+			arc.left.parent = replacement
+
+		if arc.previous != None:
+			arc.previous.next = arc.next
+		if arc.next != None:
+			arc.next.previous = arc.previous
+
+		arc = None
+		self.root = self.FixTree()
+
+		return replacement
+
+	def ListArcs(self, arc=DEFAULT):
+
+		if arc == None:
+			return []
+
+		if arc == DEFAULT:
+			arc = self.root
+
+		out = []
+
+		out = out + self.ListArcs(arc.left)
+		out.append(arc)
+		out = out + self.ListArcs(arc.right)
+
+		return out
+
+	# generates a pdf file containing a plot of the tree, via the graphviz package
+	def PlotTree(self, filename=DEFAULT):
+		
+		if filename == DEFAULT:
+			filename = time.time()
+
+		arcs = self.ListArcs()
+		dot = graphviz.Digraph()
+
+		for arc in arcs:
+			dot.node(f"{arc.focus[0]},{arc.focus[1]}")
+
+			if arc.left != None:
+				dot.edge(f"{arc.focus[0]},{arc.focus[1]}",
+						 f"{arc.left.focus[0]},{arc.left.focus[1]}")
+
+			if arc.right != None:
+				dot.edge(f"{arc.focus[0]},{arc.focus[1]}",
+						 f"{arc.right.focus[0]},{arc.right.focus[1]}")
+
+		file_path = f"graphviz_outputs/fortune_tree_{filename}"
+		dot.render(file_path, cleanup=True)
 
 
 class BeachLine(FortuneTree):
-    def __init__(self, site):
-        FortuneTree.__init__(self, Arc(site))
-    
-    def GetBreakpoints(self, sweepline):
-        breakpoints = []
-        # TODO: figure this shit out
-        return breakpoints
+	def __init__(self, site):
+		FortuneTree.__init__(self, Arc(site))
+
+	def GetBreakpoints(self, sweepline):
+		breakpoints = []
+		# TODO: figure this shit out
+		return breakpoints
 
 
 
--- main.py
+++ main.py
@@ -9,7 +9,22 @@ import BeachLine
 
 start_time = time.time()
 
+arc = BeachLine.Arc([0,0])
+tree = BeachLine.FortuneTree(arc)
 
+for i in range(1,11):
+	newarc = BeachLine.Arc([i,0])
+	tree.InsertAfter(arc, newarc)
+	tree.PlotTree(filename=f"adding arc {i}")
+	arc = newarc
+
+arc = tree.Min()
+
+while arc.next != None:
+	next_arc = arc.next
+	tree.Delete(arc)
+	tree.PlotTree(filename=f"deleted arc {arc.focus[0]}")
+	arc = next_arc
 
 
 # trees = 1
@@ -19,23 +34,23 @@ start_time = time.time()
 #     print("==============================================")
 #     print(f"TREE {i}:")
 #     print("----------------------------------------------")
-    
+	
 #     my_values = range(size)
 #     my_values = random.sample(my_values, k=size)
 
 #     a = AVLTree.AVLTree(values=my_values)
-    
+	
 #     a.print()
 #     a.generate_plot(show=(trees == 1))
 #     ancestors = a.list_ancestors(a.search(50))
-    
+	
 #     for node in ancestors:
 #         print(node.value)
 
 #     print("==============================================")
 #     print(f"TREE {i} after deleting node 50:")
 #     print("----------------------------------------------")
-    
+	
 #     a.delete(50)
 #     a.print()
 #     a.generate_plot(show=(trees == 1))
